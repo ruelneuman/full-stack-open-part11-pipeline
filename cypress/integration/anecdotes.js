ed204806eb840905d99d69a190c4fcf9c06593c1
Ruel Neuman <ruelneuman@gmail.com>
2021-08-16 18:55:41 -0700

    change server port to 3001

--- cypress/integration/anecdotes.js
+++ cypress/integration/anecdotes.js
@@ -1,6 +1,6 @@
 describe('Anecdotes', function() {
   it('front page can be opened', function() {
-    cy.visit('http://localhost:3000');
+    cy.visit('http://localhost:3001');
     cy.contains('Anecdotes');
     cy.contains('Create New');
   });
